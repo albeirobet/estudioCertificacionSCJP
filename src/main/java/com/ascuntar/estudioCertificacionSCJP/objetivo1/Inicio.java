55640b2d157e45cb52e36fc4e4ac4efb723ed07b
albeirobet <albeirobet@albeiro-ubuntu>
2014-02-26 21:48:49 -0500

    Inicializando el proyecto para el estudio de Java Programmer SE 6

---
+++ src/main/java/com/ascuntar/estudioCertificacionSCJP/objetivo1/Inicio.java
@@ -0,0 +1,9 @@
+package com.ascuntar.estudioCertificacionSCJP.objetivo1;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenido al Curso Java Programmer");
+	}
+
+}
