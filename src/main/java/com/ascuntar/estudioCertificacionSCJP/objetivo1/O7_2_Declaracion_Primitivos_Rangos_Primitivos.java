d8cf7e6a0fa3253851c33a3ccc47136bc29b215c
albeirobet <albeirobet@albeiro-ubuntu>
2014-03-04 21:06:46 -0500

    Continuando en declaracion de primitivos

---
+++ src/main/java/com/ascuntar/estudioCertificacionSCJP/objetivo1/O7_2_Declaracion_Primitivos_Rangos_Primitivos.java
@@ -0,0 +1,29 @@
+package com.ascuntar.estudioCertificacionSCJP.objetivo1;
+
+public class O7_2_Declaracion_Primitivos_Rangos_Primitivos {
+
+	/**
+	Las variables primitivas pueden ser declaradas como variables de clase
+	(estaticas), instancias de variables, parametros de metodos o variables
+	locales. Se puede declarar uno o mas primitivos, del mismo tipo de
+	primitivo, en una sola linea. 
+	 */
+	byte b;
+	boolean miBooleanPrimitivo;
+	int x, y, z; //declaramos 3 variables primitivas de tipo int
+	
+	/**
+	Para el examen actual, se puede pasar por alto algunos de los detalles,
+	pero aun es importante entender que para los tipos enteros la secuencia
+	de menor a mayor es: byte, short, int, long y que los double son mayor
+	que float.
+	
+	Todos los seis tipos en Java estan hechos de un numero de 8 bit bytes,
+	y son con signo, significa que pueden ser negativos o positivos. El bit
+	mas significativo (bit mas a la izquierda) es usuado para representar el
+	signo, donde un 1 significa negativo y 0 positivo. El resto de los bits
+	representa el valor usando notacion complemento a 2.
+	
+	
+	 */
+}
